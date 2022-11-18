17718385496594ecd9374d537fc6182ffc242734
Saf1u <a_safwa@live.concordia.ca>
2022-11-17 21:57:52 -0500

    first commit

--- cacheman/cacheManClient.go
+++ cacheman/cacheManClient.go
@@ -12,12 +12,12 @@ type Client struct {
 	Address string
 }
 
-//BuildClient returns a cacheMan client to access a cache at the passed address
+// BuildClient returns a cacheMan client to access a cache at the passed address
 func BuildClient(address string) *Client {
 	return &Client{Address: address}
 }
 
-//Ping to check if server is live returns error if unavailable
+// Ping to check if server is live returns error if unavailable
 func (c *Client) Ping() error {
 	url := fmt.Sprint("http://", c.Address, "/ping")
 	resp, err := http.Get(url)
@@ -30,7 +30,7 @@ func (c *Client) Ping() error {
 	return nil
 }
 
-//Put adds the passed value into the cache and evicts records to make space
+// Put adds the passed value into the cache and evicts records to make space
 func (c *Client) Put(key string, value []byte) error {
 	url := fmt.Sprint("http://", c.Address, "/", key)
 	resp, err := http.Post(url, "-", bytes.NewReader(value))
@@ -40,10 +40,11 @@ func (c *Client) Put(key string, value []byte) error {
 	if resp.StatusCode != 200 {
 		return errors.New("could not add record")
 	}
+	resp.Body.Close()
 	return nil
 }
 
-//Get fetches stored record from cache
+// Get fetches stored record from cache
 func (c *Client) Get(key string) ([]byte, error) {
 	url := fmt.Sprint("http://", c.Address, "/", key)
 	resp, err := http.Get(url)
@@ -54,6 +55,7 @@ func (c *Client) Get(key string) ([]byte, error) {
 		return nil, errors.New("could not get record")
 	}
 	data, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
 	if err != nil {
 		panic(err)
 	}
