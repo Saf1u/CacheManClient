9028c27250176f6da852b910926051f5a2aa08ec
Saf1u <a_safwa@live.concordia.ca>
2022-11-17 20:52:58 -0500

    first commit

--- cacheManClient.go
+++ cacheManClient.go
@@ -12,10 +12,12 @@ type Client struct {
 	Address string
 }
 
+//BuildClient returns a cacheMan client to access a cache at the passed address
 func BuildClient(address string) *Client {
 	return &Client{Address: address}
 }
 
+//Ping to check if server is live returns error if unavailable
 func (c *Client) Ping() error {
 	url := fmt.Sprint("http://", c.Address, "/ping")
 	resp, err := http.Get(url)
@@ -28,6 +30,7 @@ func (c *Client) Ping() error {
 	return nil
 }
 
+//Put adds the passed value into the cache and evicts records to make space
 func (c *Client) Put(key string, value []byte) error {
 	url := fmt.Sprint("http://", c.Address, "/", key)
 	resp, err := http.Post(url, "-", bytes.NewReader(value))
@@ -40,11 +43,12 @@ func (c *Client) Put(key string, value []byte) error {
 	return nil
 }
 
+//Get fetches stored record from cache
 func (c *Client) Get(key string) ([]byte, error) {
 	url := fmt.Sprint("http://", c.Address, "/", key)
 	resp, err := http.Get(url)
 	if err != nil {
-		return nil, errors.New("could not add record")
+		return nil, errors.New("could not get record")
 	}
 	if resp.StatusCode != 200 {
 		return nil, errors.New("could not get record")
